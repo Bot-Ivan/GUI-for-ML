e76857a41f8ccad0f1bd5912c4acd81ce8244452
IvanF <60158053+Bot-Ivan@users.noreply.github.com>
2023-12-30 11:49:04 -0500

    updated mainWindow
    -changed names
    -updated positioning logic

--- mainWindow.java
+++ mainWindow.java
@@ -1,18 +1,35 @@
+import java.awt.Color;
+import java.math.*;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.border.Border;
 
 public class mainWindow {
     public static void main(String[] args) {
         // JFrame = a GUI window to add components to
-        JFrame frame = new JFrame();
-        frame.setTitle("ML gui");//change this later
-        frame.setSize(1000,1000);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JFrame mainWindow = new JFrame();
+        mainWindow.setTitle("ML gui");//change this later
+        //#region
+        mainWindow.setSize(1000,1000);
+        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainWindow.setLayout(null);
+        Border titleBorder = BorderFactory.createLineBorder(Color.red,1);
         
 
-        ImageIcon titleIcon = new ImageIcon("Images/titleIcon.png");
+        ImageIcon titleIcon = new ImageIcon("Images/titleIcon.png"); 
         JLabel titleLabel = new JLabel();
+
+        int titleLabel_width = 200;
+        int titleLabel_height = 200;
+
+        int titleLabel_xpos =  mainWindow.getWidth() / 2 - titleLabel_width/2;
+        int titleLabel_ypos = (int)(mainWindow.getHeight() - (mainWindow.getHeight() * 0.95));
+
+
+        titleLabel.setBorder(titleBorder);
+        titleLabel.setBounds(titleLabel_xpos, titleLabel_ypos, titleLabel_width, titleLabel_height);
         titleLabel.setText("Emotional Speech Recognition");
         titleLabel.setIcon(titleIcon);
         titleLabel.setHorizontalTextPosition(JLabel.CENTER);
@@ -20,7 +37,12 @@ public class mainWindow {
         titleLabel.setHorizontalAlignment(JLabel.CENTER);
         titleLabel.setVerticalAlignment(JLabel.TOP);
 
-        frame.setVisible(true);
-        frame.add(titleLabel);
+        JLabel lblFileExplorer = new JLabel();
+        lblFileExplorer.setText("File Explorer");
+
+
+        mainWindow.setVisible(true);
+        mainWindow.add(titleLabel);
+        //frame.add(lblFileExplorer);
     }
 }
